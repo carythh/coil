678b722185cb1da18977b30d555866a2733a72e9
Colin White <colin@colinwhite.me>
2021-05-27 18:07:38 -0500

    Update AGP.

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.extra.apply {
-    set("androidPlugin", "com.android.tools.build:gradle:7.0.0-beta02")
+    set("androidPlugin", "com.android.tools.build:gradle:7.0.0-beta03")
     set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.15.1")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
